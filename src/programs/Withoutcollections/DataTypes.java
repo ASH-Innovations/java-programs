59dd37997b5ab01dea23bdd0a687378024875675
srihari201 <srihariadada19978@gmail.com>
2024-10-30 06:27:22 +0530

    Integer dataTypes

---
+++ src/programs/Withoutcollections/DataTypes.java
@@ -0,0 +1,16 @@
+package programs.Withoutcollections;
+
+public class DataTypes {
+	public static void main(String[] args) {
+		byte byteValue = 101;
+		short shortValue = 1234;
+		int intValue = 12345;
+		long longValue = 123456789875l;
+		System.out.println("byteValue is: " + byteValue);
+		System.out.println("shortValue is: " + shortValue);
+		System.out.println("intValue is: " + intValue);
+		System.out.println("longValue is: " + longValue);
+
+	}
+
+}
