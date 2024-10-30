f69e6f107d1f283719c2a7e7bbbacaa476fe6bd3
srihari201 <srihariadada19978@gmail.com>
2024-10-30 06:58:39 +0530

    added size of the Integer Data types

--- src/programs/Withoutcollections/DataTypes.java
+++ src/programs/Withoutcollections/DataTypes.java
@@ -2,10 +2,10 @@ package programs.Withoutcollections;
 
 public class DataTypes {
 	public static void main(String[] args) {
-		byte byteValue = 101;
-		short shortValue = 1234;
-		int intValue = 12345;
-		long longValue = 123456789875l;
+		byte byteValue = 101;// 1 byte
+		short shortValue = 1234;//2 bytes
+		int intValue = 12345;//4 bytes
+		long longValue = 123456789875l;//8 bytes
 		System.out.println("byteValue is: " + byteValue);
 		System.out.println("shortValue is: " + shortValue);
 		System.out.println("intValue is: " + intValue);
