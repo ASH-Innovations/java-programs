d5d037caaad836cd5b0608da556dca0055440189
srihari201 <srihariadada19978@gmail.com>
2024-11-02 12:04:51 +0530

    Revert "added floating values"
    This reverts commit 2bff6ca45bfcf417baf2535be6ac7547fb660811.

--- src/programs/Withoutcollections/DataTypes.java
+++ src/programs/Withoutcollections/DataTypes.java
@@ -3,18 +3,13 @@ package programs.Withoutcollections;
 public class DataTypes {
 	public static void main(String[] args) {
 		byte byteValue = 101;// 1 byte
-		short shortValue = 1234;// 2 bytes
-		int intValue = 12345;// 4 bytes
-		long longValue = 123456789875l;// 8 bytes
+		short shortValue = 1234;//2 bytes
+		int intValue = 12345;//4 bytes
+		long longValue = 123456789875l;//8 bytes
 		System.out.println("byteValue is: " + byteValue);
 		System.out.println("shortValue is: " + shortValue);
 		System.out.println("intValue is: " + intValue);
 		System.out.println("longValue is: " + longValue);
-
-		float floatValue = 9.9f;
-		double doubleValue = 9.88;
-		System.out.println("floatValue is: " + floatValue);
-		System.out.println("doubleValue is: " + doubleValue);
 
 	}
 
