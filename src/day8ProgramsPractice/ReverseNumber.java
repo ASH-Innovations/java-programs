2aa983d58afcf19325060d8006e90c0747261c5e
srihari201 <srihariadada19978@gmail.com>
2025-05-20 23:09:51 +0530

    added syso

--- src/day8ProgramsPractice/ReverseNumber.java
+++ src/day8ProgramsPractice/ReverseNumber.java
@@ -2,13 +2,17 @@ package day8ProgramsPractice;
 
 public class ReverseNumber {
 	public static void reverseNumber(int number) {
+		System.out.println("Execution starts from main method by JVM");
+		System.out.println("Original Number is: " + number);
 		int rev = 0;
 		while (number != 0) {
 			rev = rev * 10 + number % 10;
 			number = number / 10;
 
 		}
-		System.out.println(rev);
+		System.out.println("Reverse number is: " + rev);
+		System.out.println("Execution starts from main method by JVM");
+
 	}
 
 	public static void main(String[] args) {
