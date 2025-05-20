cf1c533a22134a23e7319fe10dbf42c711931843
srihari201 <srihariadada19978@gmail.com>
2025-05-20 19:29:26 +0530

    Day7PracticeJavaPrgramsReverseAString

--- src/day8ProgramsPractice/ReverseNumber.java
+++ src/day8ProgramsPractice/ReverseNumber.java
@@ -1,5 +1,17 @@
 package day8ProgramsPractice;
 
 public class ReverseNumber {
+	public static void reverseNumber(int number) {
+		int rev = 0;
+		while (number != 0) {
+			rev = rev * 10 + number % 10;
+			number = number / 10;
 
+		}
+		System.out.println(rev);
+	}
+
+	public static void main(String[] args) {
+		reverseNumber(678);
+	}
 }
