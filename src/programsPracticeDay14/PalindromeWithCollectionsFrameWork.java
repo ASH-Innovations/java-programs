792c85dca0192cf646df1e1bdd1a4e9c9dc96105
sradada <srihari.adadaa@gmail.com>
2025-06-28 16:02:10 +0530

    day14Practice on leadcomments

--- src/programsPracticeDay14/PalindromeWithCollectionsFrameWork.java
+++ src/programsPracticeDay14/PalindromeWithCollectionsFrameWork.java
@@ -12,14 +12,19 @@ public class PalindromeWithCollectionsFrameWork {
 		String org_String = "ASA";
 
 		List<Character> arrayList = new ArrayList<>();
+		//Convert String to charArray and pass to collection
 
 		for (Character value : org_String.toCharArray()) {
 			arrayList.add(value);
 		}
+		
+		//Reverse the collection of characters
 
 		List<Character> reverseList = new ArrayList<>(arrayList);
 		Collections.reverse(reverseList);
 		System.out.println("Reversed List: " + reverseList);
+		
+		//Converting back the collection of reversed characters to String
 
 		StringBuffer stringBuilder = new StringBuffer();
 		for (Character s : reverseList) {
