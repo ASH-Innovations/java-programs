41d9e7ad866f6a6470fcf42ee59c223b3d0492fe
Srihari Adada <srihari.adada@kanini.com>
2025-07-05 12:10:02 +0530

    ExcpetionHandling

--- src/package2AccesssModifiers/copy/ClassB.java
+++ src/package2AccesssModifiers/copy/ClassB.java
@@ -7,9 +7,9 @@ public class ClassB {
 	public static void main(String[] args) {
 		ClassA ref = new ClassA();
 		System.out.println(ref.publicVariable);
-		System.out.println(ref.defaultVariable);
-		System.out.println(ref.protectedVariable);
-		System.out.println(ref.privateVariable);
+		System.out.println(ref.publicVariable);
+		System.out.println(ref.publicVariable);
+		System.out.println(ref.publicVariable);
 
 	}
 
